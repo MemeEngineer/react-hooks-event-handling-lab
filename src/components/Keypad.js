170585e4fae0c8861f1bd73a4599d348f0678083
Johnny Wu <johnnyw131@gmail.com>
2021-12-14 20:57:31 -0500

    commit tree 7b92e3649575b0678ce38c227493040eaafea3c4

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Change() {
+  console.log("Entering password...");
+}
+
+function Keypad() {
+  console.log("Entering Password..");
+  return (
+    <div>
+      <input type="password" onChange={Change} />
+    </div>
+  );
+}
+
+export default Keypad;
